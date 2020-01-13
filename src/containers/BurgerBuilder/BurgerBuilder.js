2a55c34f4b4e27bd9972fc168729ff6e9c3fe6b0
stevechanvii <dche0007@student.monash.edu>
2020-01-14 01:11:03 +1100

    pass parameters in router

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -53,6 +53,7 @@ class BurgerBuilder extends Component {
     }
 
     componentDidMount() {
+        console.log(this.props);
         axios.get('/ingredients.json').then(response => {
             // console.log(response.data);
             this.setState({
@@ -114,30 +115,41 @@ class BurgerBuilder extends Component {
 
     purchaseContinueHandler = () => {
         // alert('You continue');
-        this.setState({ loading: true });
-        const order = {
-            ingredients: this.state.ingredients,
-            price: this.state.totalPrice,
-            customer: {
-                name: 'Steve',
-                address: {
-                    street: 'Monash',
-                    postCode: '3168',
-                    suburb: 'Clayton',
-                },
-                email: 'danyangvii@gmail.com',
-            },
-            deliveryMethod: 'fastest',
+        // this.setState({ loading: true });
+        // const order = {
+        //     ingredients: this.state.ingredients,
+        //     price: this.state.totalPrice,
+        //     customer: {
+        //         name: 'Steve',
+        //         address: {
+        //             street: 'Monash',
+        //             postCode: '3168',
+        //             suburb: 'Clayton',
+        //         },
+        //         email: 'danyangvii@gmail.com',
+        //     },
+        //     deliveryMethod: 'fastest',
+        // }
+
+        // axios.post('/orders.json', order).then(response => {
+        //     console.log(response);
+        //     this.setState({ loading: false, purchasing: false });
+        // }).catch(error => {
+        //     console.log(error);
+        //     this.setState({ loading: false, purchasing: false });
+        // });
+
+        const queryParams = [];
+        for (let i in this.state.ingredients ){
+            queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
 
-        axios.post('/orders.json', order).then(response => {
-            console.log(response);
-            this.setState({ loading: false, purchasing: false });
-        }).catch(error => {
-            console.log(error);
-            this.setState({ loading: false, purchasing: false });
+        const queryString = queryParams.join('&'); 
+        // switch the page and push a new page onto that stack of pages.
+        this.props.history.push({
+            pathname: '/checkout',
+            search: '?' + queryString,
         });
-
     }
 
     render() {
--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
 import CheckoutSummary from '../../components/Order/CheckoutSummary/CheckoutSummary';
 
@@ -11,10 +11,42 @@ class Checkout extends Component {
             bacon: 1,
         }
     }
+
+    componentDidMount() {
+        const query = new URLSearchParams(this.props.location.search);
+        const ingredients = {};
+        /**
+         * The for...of statement creates a loop iterating over iterable objects, 
+         * including: built-in String, Array, array-like objects, 
+         * An object implementing URLSearchParams can directly be used in a for...of structure
+         * 
+         * The for...in statement iterates over all enumerable properties of an object 
+         * that are keyed by strings (ignoring ones keyed by Symbols), including inherited 
+         * enumerable properties.
+         */
+        for(let param of query){
+            // + can convert string to number
+            ingredients[param[0]] = +param[1];
+        }
+        this.setState({ingredients: ingredients});
+        
+    }
+
+    checkoutCanceledHandler = () => {
+        this.props.history.goBack();
+    }
+
+    checkoutContinuedHandler =() => {
+        this.props.history.replace('/checkout/contact-data');
+    }
+
     render() {
         return (
             <div>
-                <CheckoutSummary ingredients={this.state.ingredients} />
+                <CheckoutSummary
+                    ingredients={this.state.ingredients}
+                    checkoutCanceled={this.checkoutCanceledHandler}
+                    checkoutContinued={this.checkoutContinuedHandler} />
             </div>
         );
     }
