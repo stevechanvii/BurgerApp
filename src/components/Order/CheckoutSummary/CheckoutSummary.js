d6c572315a427d8cf30758d6553a61a09a9f49f9
stevechanvii <dche0007@student.monash.edu>
2020-01-13 23:23:30 +1100

    set router

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,22 +1,23 @@
 import React from 'react';
+import { } from 'react-router-dom';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
-import classes from './checkoutSummary.css';
+import classes from './CheckoutSummary.css';
 
 const checkoutSummary = (props) => {
     return (
         <div className={classes.CheckoutSummary}>
             <h1>We hope it tastes well</h1>
-            <div style={{width: '300%', margin: 'auto'}}>
+            <div style={{ width: '100%', margin: 'auto' }}>
                 <Burger ingredients={props.ingredients} />
             </div>
-            <Button 
-            btnType="Danger"
-            clicked={}>Cancel</Button>
-            <Button 
-            btnType="Success"
-            clicked={}>Continue</Button>
+            <Button
+                btnType="Danger"
+                clicked>Cancel</Button>
+            <Button
+                btnType="Success"
+                clicked>Continue</Button>
         </div>
     );
 }
