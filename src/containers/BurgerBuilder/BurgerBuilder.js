b6b1cddcab45584963645ef8b5b2d92ce471eb2c
stevechanvii <dche0007@student.monash.edu>
2020-01-23 23:36:40 +1100

    combine multipul reducers

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -197,9 +197,9 @@ class BurgerBuilder extends Component {
 // map the redux global state to local state
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients,
-        price: state.totalPrice,
-        error: state.error
+        ings: state.burgerBuilder.ingredients,
+        price: state.burgerBuilder.totalPrice,
+        error: state.burgerBuilder.error
     }
 }
 
--- src/store/reducers/order.js
+++ src/store/reducers/order.js
@@ -34,3 +34,5 @@ const reducer = (state = initState, action) => {
             return state;
     }
 }
+
+export default reducer;
