8113de3470bac86709286ec7243620321891af8c
stevechanvii <dche0007@student.monash.edu>
2020-01-30 00:05:16 +1100

    direct to root when unknow url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, Switch, withRouter } from 'react-router-dom';
+import { Route, Switch, withRouter, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import Layout from './hoc/Layout/Layout';
@@ -14,21 +14,44 @@ class App extends Component {
   componentDidMount() {
     this.props.onTryAutoSignup();
   }
-
   render() {
+    
+    // For unauthenticated users
+    let routes = (
+      <Switch>
+        <Route path="/auth" component={Auth} />
+        <Route path="/" exact component={BurgerBuilder} />
+        {/* for unknow url just redirect to root */}
+        <Redirect to="/" />
+      </Switch>
+    );
+
+    // For authenticated users
+    if (this.props.isAuthenticated) {
+      routes = (
+        <Switch>
+          <Route path="/checkout" component={Checkout} />
+          <Route path="/orders" component={Orders} />
+          <Route path="/logout" component={Logout} />
+          <Route path="/" exact component={BurgerBuilder} />
+          <Redirect to="/" />
+        </Switch>
+      );
+    }
+
     return (
       <div>
         <Layout>
-          <Switch>
-            <Route path="/checkout" component={Checkout} />
-            <Route path="/orders" component={Orders} />
-            <Route path="/auth" component={Auth} />
-            <Route path="/logout" component={Logout} />
-            <Route path="/" exact component={BurgerBuilder} />
-          </Switch>
+          {routes}
         </Layout>
       </div>
     );
+  }
+}
+
+const mapStateToProps = state => {
+  return {
+    isAuthenticated: state.auth.token !== null,
   }
 }
 
@@ -43,4 +66,4 @@ const mapDispatchToProps = dispatch => {
  * Connect() can prevent the routing-related props to be passed to the component. 
  * withRouter fixes that as it ensures that the routing props (for history etc) are passed down. 
  */
-export default withRouter(connect(null, mapDispatchToProps)(App));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(App));
